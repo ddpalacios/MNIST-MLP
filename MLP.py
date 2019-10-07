0957f372b9dc37e23cc13818ee9d3af521b86cfa
daniel <palaciosdanieldario@gmail.com>
2019-10-07 10:58:32 -0500

    Added feed forward instructions for input -> hidden and hidden -> output

--- MLP.py
+++ MLP.py
@@ -18,11 +18,12 @@ class MultiLayerPerceptron(object):
 
         # Initialize weights to form MLP model
         # input -- > hidden
-        self.weights_hidden = np.random.rand(n_features, self.hidden_units)  # (784 , 100)
+        self.weights_hidden = self.random.normal(loc=0.0, scale=0.1,
+                                                 size=(n_features, self.hidden_units))  # (784 , 100)
         self.bias_hidden = np.zeros(self.hidden_units)  # (100, )
 
         # hidden -- > output
-        self.weigths_output = np.random.rand(self.hidden_units, n_output)  # (100, 784)
+        self.weigths_output = self.random.normal(loc=0.0, scale=0.1, size=(self.hidden_units, n_output))
         self.bias_output = np.zeros(n_output)  # (10, )
 
         # Lets print out what our model may look like
@@ -41,19 +42,28 @@ class MultiLayerPerceptron(object):
             if self.shuffle:  # Shuffles indices
                 self.random.shuffle(indices)
 
-            for start_idx in range(0, indices.shape[0] - \
-                                      self.mini_batch_size + \
-                                      1, self.mini_batch_size):
-                batch_idx = indices[start_idx:start_idx + \
-                                              self.mini_batch_size]
+            for start_idx in range(0, indices.shape[0], self.mini_batch_size):
+                batch_idx = indices[start_idx:start_idx + self.mini_batch_size]  # batch idx = [1: 60000]
 
-                print(batch_idx)
+                net1, out1, net2, out2 = self.forward(X[batch_idx])
+
+    def forward(self, X):
+        # FEED FORWARD DATA
+
+        # ACTIVATE Input --> hidden
+        net1 = np.dot(X, self.weights_hidden) + self.bias_hidden
+        out1 = self.sigmoid(net1)
+
+        # ACTIVATE hidden --> output
+        net2 = np.dot(out1, self.weigths_output) + self.bias_output
+        out2 = self.sigmoid(net2)
+
+        self.print_Live_Forward_activations(out1, out2)
+
+        return net1, out1, net2, out2
 
     def sigmoid(self, z):
         return 1 / (1 + np.exp(-z))
-
-    def forward(self):
-        pass
 
     def onehot(self, labels, n_classes):
         one_hot = np.zeros((n_classes, labels.shape[0]))  # (10, 6000)
@@ -76,3 +86,6 @@ class MultiLayerPerceptron(object):
                 self.bias_hidden.shape,
                 self.weigths_output.shape,
                 self.bias_output.shape))
+
+    def print_Live_Forward_activations(self, out1, out2):
+        print("Activation from Input -> Hidden {}\n\nActivation from Hidden -> Output {}\n\n".format(out1, out2))
